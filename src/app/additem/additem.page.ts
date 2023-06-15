beac9ed7a37564ab6e566f3d92d317ee7c3a5a1d
RaudiesM <raudiesm@gmail.com>
2023-06-15 11:03:54 +0200

    updated AddItem

--- src/app/additem/additem.page.ts
+++ src/app/additem/additem.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GetDataService } from '../services/get-data.service';
+import { datastructure } from '../add-structure/add-structure.page';
+
 
 @Component({
   selector: 'app-additem',
   templateUrl: './additem.page.html',
   styleUrls: ['./additem.page.scss'],
 })
-export class AdditemPage implements OnInit {
+export class AdditemPage{
+  listOfStructures : datastructure[] = [];
+  currentStructure : number = -1;
 
-  constructor() { }
-
+  constructor(public getData : GetDataService) {}
   ngOnInit() {
-  }
-
+    // Die Daten der Sammlungen laden
+    this.loadCollections();
+    }
+   async loadCollections(): Promise<void> {
+    try {
+    this.listOfStructures = await this.getData.loadCollections();
+    // Notify the calling object that the operation is complete
+    this.onCollectionsLoaded();
+    } catch (error) {
+    console.error(error);
+    }
+    }
+   onCollectionsLoaded(): void {
+    console.log('Daten über die Sammlungen wurden geladen');
+    }
 }
